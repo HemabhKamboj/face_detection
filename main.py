5a97aad15278c4826d8e84b0c307b577ebd914e9
Hemabh Kamboj <32740899+HemabhKamboj@users.noreply.github.com>
2018-07-06 17:43:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ while True:
 	#Detecting and highlighting faces in the frame
 	faces= face_cascade.detectMultiScale(gray, scaleFactor=1.05, minNeighbors=5, minSize=(40,40))
 	for (x,y,w,h) in faces:
-		cv2.rectangle(frame, (x,y) ,(x+w,y+h), (255,0,0), 2)
+		cv2.rectangle(frame, (x,y) ,(x+w,y+h), (255,55,0), 2)
 
 	cv2.imshow("Face detect", frame)
 	
